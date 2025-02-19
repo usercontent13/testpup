21570ed8b7df192c9c6582d24b996ae6e76aa162
usercontent13 <angelica56836@gmail.com>
2025-02-19 11:55:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_KEY = os.getenv("YOUTUBE_API_KEY", "YOUR_API_KEY_HERE")
+API_KEY = os.getenv("YOUTUBE_API_KEY", "AIzaSyCsxldUFSBF02sZ3IRIOO7lu4OufY2YPHc")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgres://default:RWb8SlUH7qoY@ep-steep-salad-a4ixlxlg-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require")
 CHANNEL_IDS = ["UC_x5XG1OV2P6uZZ5FSM9Ttw", "UC295-Dw_tDNtZXFeAPAW6Aw"]  # Add your channel IDs
