25900d4ecdd4e1126119be43eed091bb62e934df
usercontent13 <angelica56836@gmail.com>
2025-02-20 05:13:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,33 @@ import os
 
 API_KEY = os.getenv("YOUTUBE_API_KEY", "AIzaSyCsxldUFSBF02sZ3IRIOO7lu4OufY2YPHc")
 DATABASE_URL = os.getenv("DATABASE_URL", "postgres://default:RWb8SlUH7qoY@ep-steep-salad-a4ixlxlg-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require")
-CHANNEL_IDS = ["UC_x5XG1OV2P6uZZ5FSM9Ttw", "UC295-Dw_tDNtZXFeAPAW6Aw"]  # Add your channel IDs
+CHANNEL_IDS = [
+    "UCeqd7iC1BqQuUW_aZ4k9qxA",  # Sofiaturner102
+    "UCAhfFe1Ne0nFIL42CYj020w",  # RileyLeggings
+    "UCn4WL8efZQsJACcyOjWbXTQ",  # CatnipFunnies
+    "UCC1sVUJ9fPKyH2krRiYJk2Q",  #Brittanybakeer
+    "UCK4egoVnZZcY2gV_176Umkg",  #Dhariaswfx
+    "UC7TJHINbQ8HRQAMGBRonBQQ",  #florina5656
+    "UCTcRuIMIDExQUmNeI077Fog",  #Angelica
+    "UC2vmSconqtLZZVb1rzLNbOw",  #PiecesofEpicCinema
+    "UC48i3oILPkUiAqv3BbehQQA",  #AliniClosetFanPage
+    "UCc9D_DBejq2ru1MrdR83MZg",  #eleven11th
+    "UCmBTL5IZjAOv190IMiiyOEg",  #Englishwithlee
+    "UCXm84TyLa6NdXXuYi4iKaPQ",  #StraightFaceChallenge2.0
+    "UClH6G6RwjlnTSpOJEfIWmqw",  #immsarah
+    "UCP9E5B1b0idCWO0QOPQM_tg",  #Petdiaries1
+    "UCrvNV9KHu33YBsxXG2g76Rw",  #piecesoffun3439
+    "UCoNufrwhyjVb8tm_K5wKfew",  #piecesofcar
+    "UCfj3egc901UleUcKU9hMbNQ",  #VelvetRides
+    "UC0P6I7ha2_Je9UhJ_gRqukA",  #PharmaNuggets
+    "UCr0eOJe29vCwjdXyd5nULtQ",  #theonegaygirl
+    "UCbaMO0MS8DTNc5LPNm0ghUg",  #GiggleLols
+    "UChHYWbwimVzrdBqRjjwA5bQ",  #ArmasAddicts
+    "UCEYmySs83j4bqouQFUyiQZw",  #EpicMemeology
+    "UC45-bPCM1rYeebVeCrJfdgw",  #CarWheelsEnthusiast
+    "UC3HcejR-bbJ-ezhbwqoh9xA",  #IslamTheGreat
+    "UCmQLQoKvdWPTVNXZ3pZ_o_g",  #SportshiftWheels
+    "UCaI9_XnTttJB9IXoK3VH_fA",  #paws_comedy
+    "UCAURm1WVyU0sN9M9bquNJAw",  #ThatOnecarGirl
+    "UCWyQEWMejaCVhVR6AXM5jZg"  #MiyaHorseRider
+]
